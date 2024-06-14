31e60d80bfea0ac4fbb5143ddfde6830aa9d513c
sophia paulin <sobiyamary622@gmail.com>
2024-06-14 19:41:58 +0530

    html

---
+++ practice/script.js
@@ -0,0 +1,16 @@
+// const heading = document.querySelector('h1');
+// console.log(heading);
+// heading.innerText = "Sophia Paulin";
+// const heading = document.getElementById("id1");
+// setTimeout( () =>{
+    // heading.innerText = "Ajay";
+// }, 2000 );
+// console.log(heading);
+// var count = 10;
+
+
+const textInput = document.getElementById("email");
+const alterCta = document.getElementById("save");
+    alterCta.addEventListener("Click", (e) => {
+        alert("Hello");
+ });
